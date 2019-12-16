44f3cd7d165b49d63dbeaad226f2ee0baf600f70
Sarada <ryoyukikazu11@gmail.com>
2019-12-16 11:11:30 +0900

    delete:メソッド削除
    不要になったメソッドを削除

--- diary1801204/src/diary/commmon/TeacherErrorCheck.java
+++ diary1801204/src/diary/commmon/TeacherErrorCheck.java
@@ -1,16 +1,5 @@
 package diary.commmon;
 
-import diary.bean.TeacherBeans;
-
 public class TeacherErrorCheck {
 
-    public boolean checkLogin(TeacherBeans teacher_beans) {
-        boolean is_login = false;
-        try {
-            if (teacher_beans.getTeacher_code() != null) is_login = true;
-        } catch (Exception e) {
-            is_login = false;
-        }
-        return is_login;
-    }
 }
